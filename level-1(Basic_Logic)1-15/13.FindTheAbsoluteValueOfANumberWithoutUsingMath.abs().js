4ac3ae1deaa83fc825a76f3d8d6474258ca7ec44
MainShakif <shakifahmed27@gmail.com>
2026-09-25 13:50:08 +0600

    day:5 practice 1 problem and revise previous problems

--- level-1(Basic_Logic)1-15/13.FindTheAbsoluteValueOfANumberWithoutUsingMath.abs().js
+++ level-1(Basic_Logic)1-15/13.FindTheAbsoluteValueOfANumberWithoutUsingMath.abs().js
@@ -2,12 +2,10 @@
  * *13. Find the absolute value of a number without using Math.abs().
  */
 
-let num = -80;
+let num = 40;
 
 if (num < 0) {
-  console.log("Absolute value is:", -num);
+  console.log(`Absolute value of ${num} is ${-num}`);
 } else {
-  console.log(
-    "This is a positive number or zero, Do not need to convert it to its positive value",
-  );
+  console.log(`Absolute value of ${num} is ${num}`);
 }
---
+++ level-1(Basic_Logic)1-15/14.FindTheLastDigitOfANumber.js
@@ -0,0 +1,4 @@
+let number = 42647;
+let result = number % 10;
+
+console.log(`last digit of: ${number} is ${result}`);
